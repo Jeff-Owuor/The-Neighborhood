500bea4b783fc53a9e6c0547e7f8208e4bed2e12
moni-que <monicahjustus@gmail.com>
2022-06-17 22:31:34 +0300

    create views

--- neighborhoodapp/views.py
+++ neighborhoodapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'all_templates/index.html')
+
+def signup(request):
+    return render(request, 'all_templates/signup.html')
+
+def signin(request):
+    return render(request, 'all_templates/signin.html')
+
+def logout(request):
+    return redirect('signin')
+
+def search(request):
+    return render(request, 'all_templates/search.html')
+
+def profile(request):
+    return render(request, 'all_templates/profile.html')
