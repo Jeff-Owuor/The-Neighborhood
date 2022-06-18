1412bec1a50a18ee272edb859f7063e14199eb48
moni-que <monicahjustus@gmail.com>
2022-06-18 16:49:05 +0300

    add more urlpatterns

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,15 @@
 from django.urls import re_path,path,include
 from .views import index,RegisterView,LoginView,UserView,LogoutView,RefreshView
+from . import views
 
 
 
 urlpatterns = [
-    re_path(r'^$', index , name='home'),
+    re_path(r'^index',views.index,name='index'),
     path('register/', RegisterView.as_view()),
+    re_path(r'^$',views.signin,name='signin'),
+    re_path(r'^signup',views.signup,name='signup'),
+    re_path(r'^logout',views.logout,name='logout'),
     path('login/',LoginView.as_view()),
     path('user/',UserView.as_view()),
     path('logout/',LogoutView.as_view()),
