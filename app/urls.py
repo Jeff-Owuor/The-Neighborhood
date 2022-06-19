7ad4f3f4debdd19c4ef9e28a5b78a1f9feb48f10
Jeff <Xavierjeff451@gmail.com>
2022-06-19 15:35:42 +0300

    search business

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,4 @@
 from django.urls import re_path,path,include
-
-from .views import index,RegisterView,LoginView,UserView,LogoutView,RefreshView
 from . import views
 
 
@@ -8,13 +6,9 @@ from . import views
 urlpatterns = [
 
     re_path(r'^index',views.index,name='index'),
-    path('register/', RegisterView.as_view()),
     re_path(r'^$',views.signin,name='signin'),
     re_path(r'^signup',views.signup,name='signup'),
     re_path(r'^logout',views.logout,name='logout'),
-    path('login/',LoginView.as_view()),
-    path('user/',UserView.as_view()),
-    path('logout/',LogoutView.as_view()),
-    path('refresh/',RefreshView.as_view()),    
+    path('search_business/', views.search_business , name='search_business'),    
     
 ]
--- app/views.py
+++ app/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render,redirect
-
-from rest_framework.views import APIView
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.authentication import get_authorization_header
-from .serializers import UserSerializer
-from rest_framework.response import Response
-from .forms import RegisterForm
+from .forms import RegisterForm,ProfileEdit,BusinessForm
 from .models import Profile,Business
-from django.contrib.auth.models import User,auth
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .authentication import create_access_token,create_refresh_token,decode_access_token,decode_refresh_token
+
 
 # Create your views here.
 
@@ -25,7 +17,7 @@ def signup(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect('signin')
     
     return render(request, 'all_templates/signup.html', {"form":form})
 
@@ -40,7 +32,7 @@ def signin(request):
         user = authenticate(request,username=username,password=password)
         
         if user is not None:
-            signin(request,user)
+            login(request,user)
             return redirect('index')
     
     return render(request,'all_templates/signin.html',context)
@@ -105,4 +97,17 @@ def business(request):
             created = Business(image=image, name=name,email=email,neighborhood=neighborhood,user=request.user)
             created.save()
             
-    return render(request, 'ip4/business.html',{"title":'Business/Events','form':form,"business":business,'user':user})
+    return render(request, 'app/business.html',{"title":'Business/Events','form':form,"business":business,'user':user})
+
+def search_business(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        business = Business.objects.filter(name__icontains=searched).all()
+        params = {
+            'searched':searched,
+            'businesses':business
+        }
+        return render(request, 'app/search.html', params)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, 'app/search.html', {'message': message})
