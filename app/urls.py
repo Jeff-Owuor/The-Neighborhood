705a149eeddecad44dc23cf3311d9f8231dfe9e4
moni-que <monicahjustus@gmail.com>
2022-06-19 11:26:22 +0300

    app/templates/all_templates/profile.html

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import re_path,path,include
 
-from .views import index,RegisterView,LoginView,UserView,LogoutView,RefreshView
+# from .views import index,RegisterView,LoginView,UserView,LogoutView,RefreshView
 from . import views
 
 
@@ -8,13 +8,10 @@ from . import views
 urlpatterns = [
 
     re_path(r'^index',views.index,name='index'),
-    path('register/', RegisterView.as_view()),
     re_path(r'^$',views.signin,name='signin'),
+    re_path(r'^search/',views.search_business,name='search'),
     re_path(r'^signup',views.signup,name='signup'),
     re_path(r'^logout',views.logout,name='logout'),
-    path('login/',LoginView.as_view()),
-    path('user/',UserView.as_view()),
-    path('logout/',LogoutView.as_view()),
-    path('refresh/',RefreshView.as_view()),    
-    
+    re_path(r'^business',views.business,name='business'),
+    re_path(r'^profile',views.profile,name='profile'),   
 ]
