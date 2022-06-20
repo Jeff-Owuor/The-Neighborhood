a83f0bc9a5a5694deca4b63731220d4bd4ad02d1
Jeff <Xavierjeff451@gmail.com>
2022-06-20 19:41:58 +0300

    Model tests

--- app/tests.py
+++ app/tests.py
@@ -30,6 +30,43 @@ class TestModels(TestCase):
         post.create_post()
         posts = Post.objects.all()
         self.assertTrue(len(posts) > 0)
+    
+    def test_save_business(self):
+        business_post = Business.objects.get(id=1)
+        business_post.create_business()
+        business_posts = Business.objects.all()
+        self.assertTrue(len(business_posts) > 0)
+        
+    def test_save_neighborhood(self):
+        hood = Neighborhood.objects.get(id=1)
+        hood.create_neighborhood()
+        hoods= Business.objects.all()
+        self.assertTrue(len(hoods) > 0)
+        
+    def test_search_business(self):
+        business = Business.objects.get(id=1)
+        business.create_business()
+        post = Business.search_project('Gym equipment')
+        self.assertTrue(len(post) > 0)
+        
+    def test_delete_business(self):
+        business = Business.objects.get(id=1)
+        business.create_business()
+        post = Business.delete_business()
+        self.assertTrue(len(post) == 0)
+    
+    def test_delete_post(self):
+        post = Post.objects.get(id=1)
+        post.create_post()
+        posts =Post.delete_post()
+        self.assertTrue(len(posts) == 0)
+        
+    def test_delete_neighborhood(self):
+        hood = Neighborhood.objects.get(id=1)
+        hood.create_post()
+        hood_posts =Neighborhood.delete_neighborhood()
+        self.assertTrue(len(hood_posts) == 0)
+
         
         
         
