9c1c57eabbe2abe6c8f07dc976accef1113f19e6
Jeff <Xavierjeff451@gmail.com>
2022-06-21 09:52:14 +0300

    conditions to the navbar

--- app/views.py
+++ app/views.py
@@ -95,11 +95,13 @@ def post(request):
 
 def postUpload(request):
     current_user  = request.user
+    hood = Neighborhood.objects.get(id=current_user.profile.neighborhood.id)
     profile_instance = Profile.objects.get(user=current_user)
     if request.method =='POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             project = form.save(commit=False)
+            project.neighborhood = hood
             project.user = profile_instance
             project.save()
         return redirect('post')
